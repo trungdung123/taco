8929c7cea1253f56978e08b577479c405b62f894
trungdung <phamrungdungasao@gmail.com>
2022-05-17 10:34:53 +0700

    kiem tra

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,28 @@
+package tacos;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@AllArgsConstructor
+@Data
+@Entity
+public class Ingredient {
+	@Id
+	private final String id;
+	private final String name;
+	@Enumerated(EnumType.STRING)
+	private final Type type;
+
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
+}
